a21a5ff1d3334c79f878ebc6878851096451ba3a
Andrey Arutiunian <andreylzmw@gmail.com>
2023-05-24 22:05:17 +0300

    test

--- tests/python/lib/http_client.py
+++ tests/python/lib/http_client.py
@@ -1,7 +1,7 @@
 from requests_toolbelt.utils import dump
 import requests
 import socket
-import urllib3
+from urllib3 import Timeout, Poolmanager
 
 from .colors import blue
 
@@ -24,15 +24,19 @@ class RawResponse:
 
 
 def send_http_request(port, uri='/', method='GET', timeout=30, **kwargs):
-    url = 'http://127.0.0.1:{}{}'.format(port, uri)
-    print("\nSending HTTP request: [{}]".format(blue(url)))
-    request = urllib3.request.Request(method=method, url=url, timeout=timeout, **kwargs)
-    opener = urllib3.request.build_opener()
-    r = opener.open(request)
+    # session = requests.session()
+    # url = 'http://127.0.0.1:{}{}'.format(port, uri)
+    # print("\nSending HTTP request: [{}]".format(blue(url)))
+    # r = session.request(method=method, url=url, timeout=timeout, **kwargs)
+    # session.close()
     # print("HTTP request debug:")
     # print("=============================")
     # print(*[i for i in dump.dump_all(r).splitlines(True)], sep="\n")
     # print("=============================")
+    url = 'http://127.0.0.1:{}{}'.format(port, uri)
+    _timeout = Timeout(connect=timeout, read=timeout)
+    http = PoolManager(timeout=_timeout)
+    r = http.request(method, url)
     return r
 
 
